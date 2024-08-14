d982130cf8d40a5b6c217b1f626e8ff48a835238
exceed96 <exceed96@naver.com>
2024-08-14 16:35:20 +0900

    [Fix] 페이지 설명 문구 수정 #18

--- src/app/reports/layout.tsx
+++ src/app/reports/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export function generateMetadata(): Metadata {
   return {
     title: "제보 - 전국 부실아파트 조회",
-    description: "부실아파트관련 소식을 제보할 수 있는 홈페이지 입니다.",
+    description: "부실아파트관련 소식을 제보할 수 있는 서비스 입니다.",
   };
 }
 
