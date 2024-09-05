44ef07f72ac5adf348558cf76be6607fbed59136
exceed96 <exceed96@naver.com>
2024-09-05 12:02:33 +0900

    [Feat] PWA 적용 #51

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const prod = process.env.NODE_ENV === "production";
+
+import runtimeCaching from "next-pwa/cache.js";
+import withPWA from "next-pwa";
+
+const nextConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: prod ? false : true,
+});
 
 export default nextConfig;
