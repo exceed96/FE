8e2d79c6776142bccf358534c039f895286cd9cf
exceed96 <exceed96@naver.com>
2024-09-05 15:05:23 +0900

    [Feat] PWA 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,79 @@ import ModalContainer from "@/components/Container/ModalContainer";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
+export const viewport = {
+  themeColor: "#ffffff",
+};
+
 export function generateMetadata(): Metadata {
   return {
     title: "전국 부실아파트 조회",
     description: "전국 부실아파트를 조회할 수 있는 서비스 입니다.",
+    manifest: "/manifest.json",
     icons: {
       icon: "./favicon.ico",
+      other: [
+        {
+          url: "/icons/splashscreens/iphone5_splash.png",
+          media:
+            "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
+          rel: "apple-touch-startup-image",
+        },
+        {
+          url: "/icons/splashscreens/iphone6_splash.png",
+          media:
+            "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)",
+          rel: "apple-touch-startup-image",
+        },
+        {
+          url: "/icons/splashscreens/iphoneplus_splash.png",
+          media:
+            "(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)",
+          rel: "apple-touch-startup-image",
+        },
+        {
+          url: "/icons/splashscreens/iphonex_splash.png",
+          media:
+            "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)",
+          rel: "apple-touch-startup-image",
+        },
+        {
+          url: "/icons/splashscreens/iphonexr_splash.png",
+          media:
+            "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)",
+          rel: "apple-touch-startup-image",
+        },
+        {
+          url: "/icons/splashscreens/iphonexsmax_splash.png",
+          media:
+            "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)",
+          rel: "apple-touch-startup-image",
+        },
+        {
+          url: "/icons/splashscreens/ipad_splash.png",
+          media:
+            "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)",
+          rel: "apple-touch-startup-image",
+        },
+        {
+          url: "/icons/splashscreens/ipadpro1_splash.png",
+          media:
+            "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)",
+          rel: "apple-touch-startup-image",
+        },
+        {
+          url: "/icons/splashscreens/ipadpro3_splash.png",
+          media:
+            "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)",
+          rel: "apple-touch-startup-image",
+        },
+        {
+          url: "/icons/splashscreens/ipadpro2_splash.png",
+          media:
+            "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)",
+          rel: "apple-touch-startup-image",
+        },
+      ],
     },
   };
 }
