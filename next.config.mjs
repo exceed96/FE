78c397ebcdedfa4ca75df86fecec1adf381b471e
exceed96 <exceed96@naver.com>
2024-09-05 22:31:44 +0900

    [Fix] next config 파일 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
-const prod = process.env.NODE_ENV === "production";
-
-import runtimeCaching from "next-pwa/cache.js";
 import withPWA from "next-pwa";
 
+const isProd = process.env.NODE_ENV === "production";
+
+const pwaConfig = withPWA({
+  dest: "public",
+  disable: !isProd,
+  runtimeCaching: [],
+});
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  disable: prod ? false : true,
-  ...nextConfig,
-});
+export default pwaConfig(nextConfig);
