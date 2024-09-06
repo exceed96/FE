f191ab624633b2ee34d79029fb819ffcbb022412
exceed96 <exceed96@naver.com>
2024-09-06 16:11:49 +0900

    [Feat] next-pwa 옵션 수정 #54

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const isProd = process.env.NODE_ENV === "production";
 const pwaConfig = withPWA({
   dest: "public",
   disable: !isProd,
+  register: true,
+  skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */
